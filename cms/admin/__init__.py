3d8b4867583bd07a07c1e921ec08ae29504611a6
madmickstar <madmickstar@users.noreply.github.com>
2020-01-12 22:35:56 +1100

    corrected static folder

--- cms/admin/__init__.py
+++ cms/admin/__init__.py
@@ -5,7 +5,8 @@ from datetime import datetime
 
 admin_bp = Blueprint('admin', __name__, 
                       url_prefix='/admin',
-                      template_folder='templates')
+                      template_folder='templates',
+                      static_folder='static')
 
 
 
